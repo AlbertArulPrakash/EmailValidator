d34d68eb27a6c8c1551ad72dfe7508abcc0db7f3
Albert Arul Prakash <albertarulprakash123@hotmail.com>
2020-04-07 06:29:03 +0530

    Base Solution Added

---
+++ EmailValidator/EmailValidator/Validators/Gmail.cs
@@ -0,0 +1,21 @@
+﻿using Aap.EmailValidator.Validators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailValidator.Validators
+{
+    class Gmail : IEmailRule
+    {
+        public int Configuration { get; set; }
+        public string DisallowedCharacters { get; set; }
+        public string AllowedCharacters { get; set; }
+
+        public bool Validate(string emailAddress)
+        {
+
+            // Default is Not valid email. 
+            return false;
+        }
+    }
+}
---
+++ EmailValidator/EmailValidator/Validators/IEmailRule.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Aap.EmailValidator.Validators
+{
+    /// <summary>
+    /// IEmailRule interface ensures the minimum standard properties that are needed for a validator
+    /// </summary>
+    public interface IEmailRule
+    {
+        int Configuration { get; set; }
+        /// <summary>
+        /// Get or Set the characters that are disallowed in a username. 
+        /// </summary>
+        string DisallowedCharacters { get; set; }
+        /// <summary>
+        /// Get or Set the characters that are allowed in a username
+        /// </summary>
+        string AllowedCharacters { get; set; }
+
+        /// <summary>
+        /// Validate method validates the email rules that are set for individual email provider. 
+        /// This method validates the allowed and disallowed characters and other username grammar that is set by the email provider. 
+        /// e.g. Username should start with a letter or a digit. 
+        /// </summary>
+        /// <param name="emailAddress">Email address to validate</param>
+        /// <returns>True if the email address pass the validation<br />False if the email address did not pass the validation</returns>
+        bool Validate(string emailAddress);
+    }
+}
